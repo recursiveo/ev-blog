d6fce6a4d6489e944b3e785e1e1dcb8657cf61a0
ronypshaji@gmail.com <Pulickal95@>
2021-12-08 11:20:35 -0500

    Alert Correction

--- static/js/EnquiryDetailsApis.js
+++ static/js/EnquiryDetailsApis.js
@@ -80,7 +80,7 @@ async function sendReplyEmail(email,id,replyText) {
         console.log(json +'response')
         getEnqDetails()
       } else {
-        alert("HTTP-Error: " + response.status)
+        alert("Can't Send Email Now. Please try again later!");
             getEnqDetails()
       }
 
